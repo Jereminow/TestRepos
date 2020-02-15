3a15dab7208d4b9e3963825773efe932b85d3f0c
Jeremi <Jerenowi@gmail.com>
2020-02-15 12:18:42 +0100

    OwO what's this?

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int owo = (int) (Math.random()*100);
+        int owoCount = 0;
+        for (int i = 0; i < owo; i++) {
+            System.out.println("OwO");
+            owoCount++;
+        }
+        System.out.println("OwO count: " + owoCount);
+    }
+}
